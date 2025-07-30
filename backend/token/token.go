e43d717f152e25f5184f8466e7c819f514d9f2c3
FredDude <kalebduchesneau@gmail.com>
2025-07-29 23:00:08 -0500

    started the interpreter

---
+++ backend/token/token.go
@@ -0,0 +1,28 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	INT       = "INT"
+	ASTERISK  = "*"
+	CHECK     = "+"
+	CHECKMATE = "#"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+)
